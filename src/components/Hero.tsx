17e5530d2e0d650dcf9c3b6611d0162eb220b4b3
rajtripathi99 <rajj.tripathii@gmail.com>
2026-02-09 23:49:09 +0530

    fixed hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
             <div className="flex flex-col pl-10 pb-6 justify-end w-1/2 gap-20">
                 <div className="space-y-4">
                     <h1 className="text-3xl font-bold">Recharge Faster, Travel Further</h1>
-                    <p className="text-lg">Join the movement toward seamless electric vehicle <br />charging with stations that are easy to find and always <br /> ready when you are.</p>
+                    <p className="text-lg text-[#4F4F4F]">Join the movement toward seamless electric vehicle <br />charging with stations that are easy to find and always <br /> ready when you are.</p>
                     <button className="flex gap-2 bg-black text-white px-4 py-3 rounded-md">Order Now <MoveRight /></button>
                 </div>
                 <div className="flex gap-4">
