6ed6b16ae27d7fbf9088f7a5a91c6c997168a97f
rajtripathi99 <rajj.tripathii@gmail.com>
2026-02-12 15:31:16 +0530

    added smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import Features from './components/Features'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import SmoothScroll from './components/SmoothScroll'
 
 const App = () => {
   return (
-    <main className="relative w-full overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <BentoGrid />
-      <Features/>
-      <CTA/>
-      <Footer/>
-    </main>
+    <>
+      <SmoothScroll />
+      <main className="relative w-full overflow-x-hidden">
+        <Navbar />
+        <Hero />
+        <BentoGrid />
+        <Features />
+        <CTA />
+        <Footer />
+      </main>
+    </>
   )
 }
 
